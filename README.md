8f7000d9fa33ecad9bc06c9a4602695cbd29bf78
김남준 <njkim@zuminternet.com>
2019-05-02 20:16:29 +0900

    initialize settings

---
+++ README.md
@@ -0,0 +1,12 @@
+# 스프링 기반 REST API 개발
+[그런 REST API로 괜찮은가?](https://www.youtube.com/watch?v=RP_f5dMoHFc)
+* 환경
+  * Java8
+  * Spring Boot2
+    * Web
+    * JPA
+    * HATEOAS
+    * REST Docs
+    * H2
+    * MySQL
+    * Lombok
---
+++ src/main/java/io/namjune/basicrestapi/BasicRestApiApplication.java
@@ -0,0 +1,13 @@
+package io.namjune.basicrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicRestApiApplication.class, args);
+    }
+
+}
