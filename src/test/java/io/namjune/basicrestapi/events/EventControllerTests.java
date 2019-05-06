4c52866c25a70987edfcf1274037f77e5640fa5a
Namjun Kim <namjunemy@gmail.com>
2019-05-06 17:25:24 +0900

    Controller Post 처리, 테스트 코드 ObjectMapping 후 post, jsonPath로 응답 확인

--- src/test/java/io/namjune/basicrestapi/events/EventControllerTests.java
+++ src/test/java/io/namjune/basicrestapi/events/EventControllerTests.java
@@ -1,5 +1,6 @@
 package io.namjune.basicrestapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -19,12 +25,32 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void 이벤트_생성_201() throws Exception {
+        Event event = Event.builder()
+            .name("REST API with Spring")
+            .description("REST API Basic")
+            .beginEnrollmentDateTime(LocalDateTime.of(2019,5,6,17,0,0))
+            .closeEnrollmentDateTime(LocalDateTime.of(2019,5,9,17,0,0))
+            .beginEventDateTime(LocalDateTime.of(2019,5,10,17,0,0))
+            .endEventDateTime(LocalDateTime.of(2019,5,13,17,0,0))
+            .basePrice(100)
+            .maxPrice(100)
+            .limitOfEnrollment(100)
+            .location("서울대입구")
+            .build();
+
         mockMvc.perform(
             post("/api/events")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .accept(MediaTypes.HAL_JSON))
-            .andExpect(status().isCreated());
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+            .andDo(print())
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("id").exists())
+        ;
     }
 }
