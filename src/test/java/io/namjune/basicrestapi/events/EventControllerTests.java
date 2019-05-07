6743f78fbee38a7f390b01f0dcf37b9827168017
Namjun Kim <namjunemy@gmail.com>
2019-05-07 18:27:05 +0900

    ObjectMapper deserialization 설정으로 BadRequest 처리(입력값(RequestDTO) 이외의 필드 포함 요청시)

--- src/test/java/io/namjune/basicrestapi/events/EventControllerTests.java
+++ src/test/java/io/namjune/basicrestapi/events/EventControllerTests.java
@@ -35,6 +35,38 @@ public class EventControllerTests {
 
     @Test
     public void 이벤트_생성_201() throws Exception {
+        EventRequestDto event = EventRequestDto.builder()
+            .name("REST API with Spring")
+            .description("REST API Basic")
+            .beginEnrollmentDateTime(LocalDateTime.of(2019, 5, 6, 17, 0, 0))
+            .closeEnrollmentDateTime(LocalDateTime.of(2019, 5, 9, 17, 0, 0))
+            .beginEventDateTime(LocalDateTime.of(2019, 5, 10, 17, 0, 0))
+            .endEventDateTime(LocalDateTime.of(2019, 5, 13, 17, 0, 0))
+            .basePrice(100)
+            .maxPrice(100)
+            .limitOfEnrollment(100)
+            .location("서울대입구")
+            .build();
+
+        mockMvc.perform(
+            post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+            .andDo(print())
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("id").exists())
+            .andExpect(header().exists(HttpHeaders.LOCATION))
+            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+            //임의로 아이디를 세팅할 수 없음. 다른 값들도 마찬가지.
+            .andExpect(jsonPath("id").value(Matchers.not(100L)))
+            .andExpect(jsonPath("free").value(Matchers.not(true)))
+            .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
+        ;
+    }
+
+    @Test
+    public void 이벤트_생성_BadRequest() throws Exception {
         Event event = Event.builder()
             .id(100L)
             .name("REST API with Spring")
@@ -58,14 +90,10 @@ public class EventControllerTests {
                 .accept(MediaTypes.HAL_JSON)
                 .content(objectMapper.writeValueAsString(event)))
             .andDo(print())
-            .andExpect(status().isCreated())
-            .andExpect(jsonPath("id").exists())
-            .andExpect(header().exists(HttpHeaders.LOCATION))
-            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-            //임의로 아이디를 세팅할 수 없음. 다른 값들도 마찬가지.
-            .andExpect(jsonPath("id").value(Matchers.not(100L)))
-            .andExpect(jsonPath("free").value(Matchers.not(true)))
-            .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
+
+            // ObjectMapper가 JSON요청을 RequestDTO로 deserialize하지 못할 때(받지 않아야 할 필드를 받아서)
+            // yml 환경설정에서 fail-on-unknown-properties를 true로 주면 BadRequest 응답으로 내려감
+            .andExpect(status().isBadRequest())
         ;
     }
 }
