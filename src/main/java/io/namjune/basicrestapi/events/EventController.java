492663e1d04dd165cbc8385235c16352ad37f6b5
Namjun Kim <namjunemy@gmail.com>
2019-05-28 23:19:30 +0900

    Revert "@SpringBootTest -> @WebMvcTest로 변경중"
    This reverts commit 69ba40889fa09adcd6d85ea49fd78c5913d29184.

--- src/main/java/io/namjune/basicrestapi/events/EventController.java
+++ src/main/java/io/namjune/basicrestapi/events/EventController.java
@@ -1,11 +1,8 @@
 package io.namjune.basicrestapi.events;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-
 import io.namjune.basicrestapi.common.ErrorsResource;
-import java.net.URI;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
@@ -22,16 +19,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
+import java.net.URI;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
 @Controller
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_UTF8_VALUE)
 @RequiredArgsConstructor
 public class EventController {
 
-    private final EventService eventService;
+    private final EventRepository eventRepository;
+    private final ModelMapper modelMapper;
     private final EventValidator eventValidator;
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody @Valid EventRequestDto eventRequestDto, Errors errors) {
+    public ResponseEntity createEvent(@RequestBody @Valid EventRequestDto eventRequestDto,
+                                      Errors errors) {
         if (errors.hasErrors()) {
             return badRequest(errors);
         }
@@ -41,14 +45,15 @@ public class EventController {
             return badRequest(errors);
         }
 
-        Event savedEvent = this.eventService.add(eventRequestDto);
+        Event event = modelMapper.map(eventRequestDto, Event.class);
+        event.updateDynamicField();
+        Event savedEvent = this.eventRepository.save(event);
 
         //HATEOAS link 추가
-        ControllerLinkBuilder selfLinkBuilder = linkTo(EventController.class)
-            .slash(savedEvent.getId());
+        ControllerLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(savedEvent.getId());
         URI createdUri = selfLinkBuilder.toUri();
 
-        EventResource eventResource = new EventResource(savedEvent);
+        EventResource eventResource = new EventResource(event);
         //self link는 매번 API 마다 추가 해야하므로 EventResource에서 공통 처리
         eventResource.add(linkTo(EventController.class).withRel("query-events"));
         eventResource.add(selfLinkBuilder.withRel("update-event"));
@@ -60,17 +65,18 @@ public class EventController {
     /**
      * 이벤트 조회
      *
+     * @param pageable
      * @param assembler 페이지를 리소스 객체로 바꿔서 링크정보를 추가할 때 유용하게 사용되는 Spring-Data-JPA가 제공하는 객체
+     * @return
      */
     @GetMapping
     public ResponseEntity queryEvents(Pageable pageable, PagedResourcesAssembler<Event> assembler) {
-        Page<Event> page = this.eventService.findAll(pageable);
+        Page<Event> page = this.eventRepository.findAll(pageable);
 
         URI location = linkTo(EventController.class).toUri();
 
         // repository에서 받아온 page를 리소스 객체로 변경
-        PagedResources<Resource<Event>> pagedResources = assembler
-            .toResource(page, e -> new EventResource(e));
+        PagedResources<Resource<Event>> pagedResources = assembler.toResource(page, e -> new EventResource(e));
         pagedResources.add(new Link("/docs/index.html#resources-events-list").withRel("profile"));
         return ResponseEntity.ok()
             .header("Location", String.valueOf(location))
