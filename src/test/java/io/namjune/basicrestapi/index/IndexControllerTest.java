ca69369d765fa418a4726ac934c3bd3bf668f6e5
Namjun Kim <namjunemy@gmail.com>
2019-06-23 16:04:34 +0900

    인덱스 컨트롤러 문서화

--- src/test/java/io/namjune/basicrestapi/index/IndexControllerTest.java
+++ src/test/java/io/namjune/basicrestapi/index/IndexControllerTest.java
@@ -1,6 +1,12 @@
 package io.namjune.basicrestapi.index;
 
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -14,6 +20,14 @@ public class IndexControllerTest extends BaseControllerTest {
         //다른 리소스에 대한 링크를 제공하기 위한 인덱스 생성 테스트
         this.mockMvc.perform(get("/api/"))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("_links.events").exists());
+            .andExpect(jsonPath("_links.events").exists()
+            )
+            .andDo(print())
+            .andDo(document("index",
+                links(
+                    linkWithRel("events").description("서비스 진입점")
+                )
+            ))
+        ;
     }
 }
